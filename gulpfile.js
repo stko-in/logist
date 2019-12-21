7120693865316463cdc5103bbaf666fee2031d58
Stas Kotlyar <staskotlyar.dev@gmail.com>
2019-12-21 18:19:16 +0200

    gulp work

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require("browser-sync").create();
+
+function logist(done) {
+  gulp.src('scss/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.reload({ stream: true }));
+  done();
+}
+
+// BrowserSync
+function sync(done) {
+  browserSync.init({
+    server: {
+		baseDir: './'
+	}
+  });
+  	gulp.watch('scss/**/*.scss', logist);
+  	gulp.watch('templates/**/*.twig').on('change', browserSync.reload);
+  done();
+}
+
+exports.default = sync;
