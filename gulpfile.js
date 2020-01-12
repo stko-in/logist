4a61db6a02c88d72e783efdae3b61e96124c4397
Stas Kotlyar <staskotlyar.dev@gmail.com>
2020-01-13 00:05:48 +0200

    begin 1

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ const browserSync = require("browser-sync").create();
 
 function logist(done) {
   gulp.src('scss/**/*.scss')
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('css/'))
-        .pipe(browserSync.reload({ stream: true }));
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({ stream: true }));
   done();
 }
 
@@ -20,8 +20,9 @@ function sync(done) {
   browserSync.init({
     proxy: "http://site"
   });
-  	gulp.watch('scss/**/*.scss', logist);
-  	gulp.watch('templates/**/*.twig').on('change', browserSync.reload);
+    gulp.watch('scss/**/*.scss', logist);
+    gulp.watch('templates/**/*.twig').on('change', browserSync.reload);
+    gulp.watch('css/**/*.css').on('change', browserSync.reload);
   done();
 }
 
